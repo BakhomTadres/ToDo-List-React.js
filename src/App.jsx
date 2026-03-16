1493d9c62e1eae7ecbf3cafb1b95fc3fd704ffc2
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-03-16 17:57:33 +0200

    Edit in files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import ToDoList from './Components/ToDoList'
-import './App.css'
+import './Style.css'
 
 function App() {
   
-
   return (
     <ToDoList />
   )
