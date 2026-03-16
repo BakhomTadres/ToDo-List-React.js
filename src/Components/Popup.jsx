d497cf5a1b77108712d978fa0ae72eb3e595328c
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-03-16 15:44:29 +0200

    Add Files

---
+++ src/Components/Popup.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+export default function ({
+  handleShow,
+  handleDeleteConfirm,
+  handleEditConfirm,
+  id,
+  type,
+  inputValue,
+}) {
+  const [nameInput, setNameInput] = useState(inputValue);
+  return (
+    <>
+      <div className="popup-box">
+        {type == "edit" ? (
+          <>
+            <h2>Edit Task</h2>
+            <input
+              required
+              id="name"
+              name="name"
+              value={nameInput}
+              onChange={(e) => setNameInput(e.target.value)}
+            />
+            <label htmlFor="name">Your Task</label>
+          </>
+        ) : (
+          <>
+            <h2 style={{ margin: "10px 0" }}>
+              Are you sure you want to delete the task?
+            </h2>
+            <p>You cannot undo deletion if you select the delete button.</p>
+          </>
+        )}
+
+        <button
+          className={type == "edit" ? "edit-btn" : "delete-btn"}
+          style={{ color: `${type == "edit" ? "#2196F3" : "#F44336"}` }}
+          onClick={() => {
+            type == "edit"
+              ? handleEditConfirm(id, nameInput)
+              : handleDeleteConfirm(id);
+          }}
+        >
+          {type == "edit" ? "Edit" : "Yes, delete"}
+        </button>
+        <button
+          className={type == "edit" ? "edit-btn" : "delete-btn"}
+          style={{ color: `${type == "edit" ? "#2196F3" : "#F44336"}` }}
+          onClick={() => handleShow(false)}
+        >
+          Cancel
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ src/Components/ToDoList.jsx
@@ -0,0 +1,205 @@
+import { use, useEffect, useState } from "react";
+import ToDo from "./ToDo";
+import Popup from "./Popup";
+function ToDoList() {
+  const [id, setId] = useState(1);
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [test, setTest] = useState("All");
+  const [show, setShow] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+  const [idDelete, setIdDelete] = useState(0);
+  const [idEdit, setIdEdit] = useState(0);
+  const [editName, setEditname] = useState("");
+  const [buttons, setButtons] = useState([
+    {
+      id: 1,
+      name: "All",
+      active: true,
+    },
+    {
+      id: 2,
+      name: "Completed",
+      active: false,
+    },
+    {
+      id: 3,
+      name: "Not Completed",
+      active: false,
+    },
+  ]);
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(savedTasks);
+    if (savedTasks.length > 0) {
+      const maxId = Math.max(...savedTasks.map((t) => t.id));
+      setId(maxId + 1);
+    } else {
+      setId(1);
+    }
+  }, []);
+  // Functions For ToDo
+  function handleDeleteToDo(id) {
+    setShow(true);
+    setIdDelete(id);
+  }
+  function handleEditToDo(id, name) {
+    setEditname(name);
+    setShowEdit(true);
+    setIdEdit(id);
+  }
+  // Functions For ToDoList
+  function handleActiveClick(id) {
+    const newButtons = buttons.map((button) => {
+      if (button.id == id) {
+        button.active = true;
+        setTest(button.name);
+      } else {
+        button.active = false;
+      }
+      return button;
+    });
+    setButtons(newButtons);
+  }
+  function handleAddClick() {
+    setTasks([...tasks, { id: id, name: inputValue, isCompleted: false }]);
+    localStorage.setItem(
+      "tasks",
+      JSON.stringify([
+        ...tasks,
+        { id: id, name: inputValue, isCompleted: false },
+      ]),
+    );
+    setId((id) => id + 1);
+    setInputValue("");
+  }
+  function handleDeleteClick(id) {
+    const tasksFilter = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(tasksFilter);
+    setShow(false);
+    localStorage.setItem("tasks", JSON.stringify(tasksFilter));
+  }
+  function handleCheckClick(id) {
+    let tasksCheck = tasks.map((t) => {
+      if (t.id == id) {
+        t.isCompleted == true
+          ? (t.isCompleted = false)
+          : (t.isCompleted = true);
+      }
+      return t;
+    });
+    setTasks(tasksCheck);
+    localStorage.setItem("tasks", JSON.stringify(tasksCheck));
+  }
+  function handleEditClick(id, name) {
+    let tasksEdit = tasks.map((t) => {
+      if (t.id == id) {
+        t.name = name;
+      }
+      return t;
+    });
+    setTasks(tasksEdit);
+    setShowEdit(false);
+    localStorage.setItem("tasks", JSON.stringify(tasksEdit));
+  }
+  const tasksList = (
+    test == "All"
+      ? tasks
+      : test == "Completed"
+        ? tasks.filter((t) => t.isCompleted)
+        : tasks.filter((t) => !t.isCompleted)
+  ).map((task) => {
+    return (
+      <ToDo
+        key={task.id}
+        id={task.id}
+        name={task.name}
+        completed={task.isCompleted}
+        handleDelete={() => handleDeleteToDo(task.id)}
+        handleEdit={() => handleEditToDo(task.id, task.name)}
+        handleCheck={() => handleCheckClick(task.id)}
+      />
+    );
+  });
+  const buttonsList = buttons.map((button) => {
+    return (
+      <button
+        onClick={() => handleActiveClick(button.id)}
+        key={button.id}
+        className={button.active ? "active" : ""}
+      >
+        {button.name}
+      </button>
+    );
+  });
+  return (
+    <>
+      {show ? (
+        <div className="overlay" onClick={() => setShow(false)}></div>
+      ) : (
+        ""
+      )}
+      {show ? (
+        <Popup
+          handleShow={setShow}
+          handleDeleteConfirm={handleDeleteClick}
+          id={idDelete}
+        />
+      ) : (
+        ""
+      )}
+      {showEdit ? (
+        <div className="overlay" onClick={() => setShowEdit(false)}></div>
+      ) : (
+        ""
+      )}
+      {showEdit ? (
+        <Popup
+          handleShow={setShowEdit}
+          handleEditConfirm={handleEditClick}
+          id={idEdit}
+          type={"edit"}
+          inputValue={editName}
+        />
+      ) : (
+        ""
+      )}{" "}
+      <div className="to-do-list">
+        <div className="top-todo-list">
+          <input
+            id="add"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            type="text"
+            required
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleAddClick();
+              }
+            }}
+          />
+          <label htmlFor="add" className="add">
+            Enter A Task
+          </label>
+          <button disabled={inputValue.length == 0} onClick={handleAddClick}>
+            Add Task
+          </button>
+        </div>
+        <div className="buttons">{buttonsList}</div>
+        <button
+          className="delete-all-btn"
+          onClick={() => {
+            setTasks([]);
+            localStorage.setItem("tasks", JSON.stringify([]));
+          }}
+        >
+          Delete All
+        </button>
+        {tasksList}
+      </div>
+    </>
+  );
+}
+export default ToDoList;
